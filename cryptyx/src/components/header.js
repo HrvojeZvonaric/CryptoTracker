8de3132c66d01d4226492f6c94e5d349ddffcdc1
Hrvoje Zvonaric <Hrvoje.zvonaric753@gmail.com>
2021-06-04 20:08:48 +0200

    commit

---
+++ cryptyx/src/components/header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const header = () => {
+    return (
+        <div className="header"> 
+
+        Cryptyx
+
+
+            
+        </div>
+    )
+}
+
+export default header
